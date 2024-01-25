165d2d54b1a74858d6ad24849baad10f7fc3ac3c
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-01-25 18:53:58 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> v;//ture = + & false = -
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+
+        if(x != 0){
+            if(x >= 0){
+                v.push(make_pair(x, 1)); // 1 -> 양수
+            }
+            else if(x < 0){
+                v.push(make_pair(-x, 0)); // 0 -> 음수
+            }
+        }
+        else{ // x == 0
+            if(v.empty()){
+                cout << "0" << '\n';
+                continue;
+            }
+            else{
+                if(v.top().second == 1){
+                    cout << v.top().first << '\n';
+                }
+                else if(v.top().second == 0){
+                    cout << -v.top().first << '\n';                    
+                }
+                v.pop();
+            }
+        }
+    }
+    return 0;
+}

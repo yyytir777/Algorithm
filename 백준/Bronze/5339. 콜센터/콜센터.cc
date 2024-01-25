7a30bd4abe47ec23beaa4ce99df44358a53479d8
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-01-25 18:49:35 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	cout << "     /~\\" << endl;
+	cout << "    ( oo|" << endl;
+	cout << "    _\\=/_" << endl;
+	cout << "   /  _  \\" << endl;
+	cout << "  //|/.\\|\\\\" << endl;
+	cout << " ||  \\ /  ||" << endl;
+	cout << "============" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+}

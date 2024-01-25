f3d0764776d6513facc54396152cb665d64007a9
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-01-25 18:49:25 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+	cout << ".  .   ." << endl;
+	cout << "|  | _ | _. _ ._ _  _" << endl;
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+}

641bbf3d5552a4f9b5a680f48948b3d0dfe58d5d
Lim Wonjae <yyytir777@gmail.com>
2024-04-07 15:04:59 +0900

    [Silver I] Title: 회의실 배정, Time: 84 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,44 +1,30 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
+#define MAX 100000
 using namespace std;
 
-#define N 100000
-vector<pair<int, int>> v;
-//끝나는 시간을 기준으로 sort -> 제일 마지막에 끝나는 시간을 기준으로 차례대로 cnt++ ....???
+pair<int, int> arr[MAX];
 
-//내림차순으로 정렬
-bool compare(pair<int, int> p1, pair<int, int> p2){
-    if(p1.first == p2.first){
-        return p1.second > p2.second;
-    }
-    return p1.first > p2.first;
-}
-
-int main(){
-    int n, start, end;
+int main() {
+    int n;
     cin >> n;
 
-
-    //배열에 시작시간 , 끝나는 시간을 pair로 입력받음
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
+        int end, start;
         cin >> start >> end;
-        v.push_back(make_pair(end, start));
+        arr[i] = make_pair(end, start);
     }
 
-    //끝나는 시간이 큰 순서대로 정렬 > 시작하는 시간이 큰 순서대로 정렬
-    sort(v.begin(), v.end());
-    
-    int start_time = v[0].first;
-    int cnt = 1;
+    sort(arr, arr+n);
 
-    for(int i = 1; i < v.size(); i++){
-        if(start_time <= v[i].second){
+    int cnt = 1;
+    int firtEndTime = arr[0].first;
+    for(int i = 1; i <= n; i++) {
+        if(firtEndTime <= arr[i].second) {
             cnt++;
-            start_time = v[i].first;
+            firtEndTime = arr[i].first;
         }
     }
 
     cout << cnt;
-    return 0;
 }

4433a8455c3223c6367298beb34aace6354c51a3
Lim Wonjae <yyytir777@gmail.com>
2024-06-09 19:34:05 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long a, b, c;
+
+long long multiply(long long i, long long j, long long k) {
+    long long result;
+    if (j==1) return i % k;
+    
+    long long tmp;
+    // 짝수일 때
+    if(j % 2 == 0) {
+        tmp = multiply(i, j / 2, k) % k;
+        result = tmp * tmp;
+    }
+    // 홀수일 때
+    else {
+        tmp = multiply(i, j / 2, k) % k;
+        result = ((tmp * tmp) % k * i) % k;
+    }
+    
+    return result % k;
+}
+
+int main() {
+    cin >> a >> b >> c;
+    cout << multiply(a % c, b, c);
+}

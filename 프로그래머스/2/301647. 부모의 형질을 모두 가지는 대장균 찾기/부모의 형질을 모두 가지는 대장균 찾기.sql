8a491fbaf7ea58fe18ce297b3acbc67a89907926
Lim Wonjae <yyytir777@gmail.com>
2024-07-16 17:39:53 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -0,0 +1,5 @@
+SELECT child.ID, child.GENOTYPE, parent.GENOTYPE AS PARENT_GENOTYPE
+FROM ECOLI_DATA child, ECOLI_DATA parent
+WHERE child.PARENT_ID = parent.ID 
+AND child.GENOTYPE & parent.GENOTYPE = parent.GENOTYPE
+ORDER BY child.ID ASC;
